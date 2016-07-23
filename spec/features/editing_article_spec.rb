9c167960cb7645eb4c704fba27974dde5b5d31d0
ChanneDesign <channebertrand@gmail.com>
2016-07-23 17:51:28 -0400

    update article

---
+++ spec/features/editing_article_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+RSpec.describe 'Editing Articles', type: :feature do
+	
+	before do
+		@article = Article.create(title: 'first article', body: 'body of first article')
+	end
+
+	scenario 'A user edits an article' do
+		visit '/'
+
+		click_link @article.title
+		click_link 'Edit Article'
+		expect(page.current_path).to eq(edit_article_path(@article))
+		fill_in 'Title', with: 'first article edited'
+		fill_in 'Body', with: 'body edited'
+		click_button 'Update Article'
+
+		expect(page).to have_content('Article has been updated')
+		expect(page.current_path).to eq(article_path(@article))
+	end
+
+	scenario 'A user fails to edit an article' do
+		visit '/'
+
+		click_link @article.title
+		click_link 'Edit Article'
+		expect(page.current_path).to eq(edit_article_path(@article))
+		fill_in 'Title', with: ''
+		fill_in 'Body', with: 'body edited'
+		click_button 'Update Article'
+
+		expect(page).to have_content('Article has not been updated')
+		expect(page).to have_content("Title can't be blank")
+		expect(page.current_path).to eq(article_path(@article))
+	end
+
+end
